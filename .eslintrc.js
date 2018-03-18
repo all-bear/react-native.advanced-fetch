e4dbf2faccdeec97d89238a2461c56753ab957e5
Oleh Biriukov <ol.biriukov@gmail.com>
2018-03-18 15:16:47 +0200

    #13 Add eslint support

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "extends": "google",
+    "env": {
+        "es6": true,
+    },
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+        },
+        "sourceType": "module",
+    },
+    "rules": {
+        "max-len": 0
+    }
+};
